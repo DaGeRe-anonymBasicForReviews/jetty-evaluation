a053eefedf9d2a64889070bb537cfb12ce7c7620
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-04-11 18:17:34 +0200

    Runnable versions for scripts on slurm

--- src/main/resources/runEvaluation.sh
+++ src/main/resources/runEvaluation.sh
@@ -1,34 +1,41 @@
+#!/bin/bash
+
 #SBATCH --cpu-freq=high-high
 #SBATCH --cpus-per-task=24
 #SBATCH --ntasks=1
 
-export PATH=/nfs/user/do820mize/maven/apache-maven-3.5.4/bin:/usr/jdk64/jdk1.8.0_112/bin/:/usr/lib64/qt-3.3/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/nfs/user/do820mize/pxz:/nfs/user/do820mize/tar-1.29/bin/bin:/nfs/user/do820mize/git/git-2.9.5/bin-wrappers
+export PATH=/nfs/user/do820mize/maven/apache-maven-3.6.3/bin:/usr/lib/jvm/java-11-openjdk-11.0.10.0.9-0.el7_9.x86_64/bin/:/usr/lib64/qt-3.3/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/nfs/user/do820mize/pxz:/nfs/user/do820mize/tar-1.29/bin/bin:/nfs/user/do820mize/git/git-2.9.5/bin-wrappers
 
 echo "Executing $benchmark on $regression"
 
 startFolder=$(pwd)
 resultFolder=$startFolder/peass-evaluation-jetty-results
 mkdir -p $resultFolder
+echo "Final results go to $resultFolder"
 
 experimentFolder=/tmp/peass-evaluation-jetty/$regression
+if [ -d $experimentFolder ]
+then
+	rm -rf $experimentFolder
+fi
 mkdir -p $experimentFolder
 cd $experimentFolder
 
-git clone https://github.com/DaGeRe/jetty-experiments
+git clone /home/sc.uni-leipzig.de/do820mize/jetty-experiments
 cd jetty-experiments
 mkdir -p jmh_results
 
 git checkout b56edf511ab4399122ea2c6162a4a5988870f479
-mvn -V -B clean install \
-	-DskipTests -Dlicense.skip -Denforcer.skip -Dcheckstyle.skip \
+mvn -V -B clean package \
+	-DskipTests -Dlicense.skip -Denforcer.skip -Dcheckstyle.skip -Djacoco.skip \
 	 -T6 -e -pl :jetty-jmh -am
 java -jar tests/jetty-jmh/target/benchmarks.jar \
 	-rff jmh_results/basic.json -rf json \
 	 -i 3 -t 3 -wi 3 $benchmark
 
 git checkout $regression
-mvn -V -B clean install \
-	-DskipTests -Dlicense.skip -Denforcer.skip -Dcheckstyle.skip \
+mvn -V -B clean package \
+	-DskipTests -Dlicense.skip -Denforcer.skip -Dcheckstyle.skip -Djacoco.skip \
 	 -T6 -e -pl :jetty-jmh -am
 java -jar tests/jetty-jmh/target/benchmarks.jar \
 	-rff jmh_results/$regression.json -rf json \
---
+++ src/main/resources/start.sh
@@ -0,0 +1,15 @@
+mkdir -p /nfs/user/do820mize/processlogs/jetty-evaluation-jmh/
+
+for line in $(cat regressions.csv | tail -n 95)
+#for i in {0..100}
+do
+	benchmark=$(echo $line | awk -F';' '{print $2}')
+	regression=$(echo $line | awk -F';' '{print $1}')
+	echo "Starting benchmark $benchmark on $regression"
+	sbatch \
+                --nice=1 \
+                --time=10-0 \
+                --output=/nfs/user/do820mize/processlogs/jetty-evaluation-jmh/"%j".out \
+		--export=benchmark=$benchmark,regression=$regression \
+		runEvaluation.sh
+done
