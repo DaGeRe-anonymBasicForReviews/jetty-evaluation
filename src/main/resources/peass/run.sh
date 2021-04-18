f21b9ff0e0826d2def2fd0723dc0c45056d78e41
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-04-18 13:40:52 +0200

    Make usable for regression-0 - reduce repetitions because of big test case

--- src/main/resources/peass/run.sh
+++ src/main/resources/peass/run.sh
@@ -4,13 +4,14 @@ version=$(git rev-parse HEAD)
 cd ..
 
 echo "Analyzing $version"
-#java -cp $PEASS_PROJECT/distribution/target/peass-distribution-0.1-SNAPSHOT.jar de.peass.debugtools.DependencyReadingContinueStarter \
-#	-dependencyfile deps_jetty.project.json -folder jetty.project/ -doNotUpdateDependencies &> dependencylog.txt
+java -cp $PEASS_PROJECT/distribution/target/peass-distribution-0.1-SNAPSHOT.jar de.peass.debugtools.DependencyReadingContinueStarter \
+	-dependencyfile deps_jetty.project.json -folder jetty.project/ -doNotUpdateDependencies &> dependencylog.txt
 
 echo "Measuring"
 java -cp $PEASS_PROJECT/distribution/target/peass-distribution-0.1-SNAPSHOT.jar de.peass.DependencyTestStarter \
 	-dependencyfile results/deps_jetty.project.json -folder jetty.project/ \
-	-iterations 10 \
-	-repetitions 1000000 \
+	-iterations 5 \
+	-repetitions 5000 \
 	-vms 200 \
-	-version $version &> measurelog.txt
+	-timeout 3 \
+	-version $version -pl ":jetty-jmh" &> measurelog.txt
