e131a7ddaceef9d4ee10457ff1531772a01ca596
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-05-06 16:38:52 +0200

    Execute class

--- src/main/resources/jmh/start.sh
+++ src/main/resources/jmh/start.sh
@@ -3,7 +3,7 @@ mkdir -p /nfs/user/do820mize/processlogs/jetty-evaluation-jmh/
 for line in $(cat regressions.csv | tail -n 95)
 #for i in {0..100}
 do
-	benchmark=$(echo $line | awk -F';' '{print $2}')
+	benchmark=$(echo $line | awk -F';' '{print $2}' | awk -F'#' '{print $1}')	
 	regression=$(echo $line | awk -F';' '{print $1}')
 	echo "Starting benchmark $benchmark on $regression"
 	sbatch \
