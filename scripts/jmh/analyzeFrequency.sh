5e3b64c4a0f02c4d00f96c124dac5da42f257493
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-02-17 11:39:32 +0100

    Fix share analysis scripts

--- scripts/jmh/analyzeFrequency.sh
+++ scripts/jmh/analyzeFrequency.sh
@@ -1,31 +1,45 @@
 #java -cp ../../target/jetty-evaluation-0.1-SNAPSHOT.jar de.dagere.peass.rtsEvaluation.ReadJMHMeasuredRegressions $1 \
 #	| grep "regression" | awk '{print $1}' | uniq > identified.txt
 
+treeFolder=$2
+if [ ! -d $treeFolder ]
+then
+	echo "TreeFolder was no folder: $treeFolder"
+	exit 1
+fi
+simpleTraceFolder=$treeFolder/simpleTraces
+if [ ! -d $simpleTraceFolder ]
+then
+	echo "SimpleTraceFolder was not existing: $simpleTraceFolder"
+	exit 1
+fi
+
 function getSum {
   awk '{sum += $1; square += $1^2} END {print sqrt(square / NR - (sum/NR)^2)" "sum/NR" "NR}'
 }
 
-for regression in $(cat identified.txt)
-do
+function getRegressionCalls {
+	regression=$1
 	selectedBenchmark=$(cat ../regressions.csv | grep $regression";" | awk -F';' '{print $2}')
 	changedMethod=$(cat ../regressions.csv | grep $regression";" | awk -F';' '{print $3}')
-	changedMethodCalls=$(cat $2/simpleTraces/root-"$selectedBenchmark"* | grep  $changedMethod | wc -l)
-	calls=$(cat $2/simpleTraces/root-$selectedBenchmark_* | wc -l)
-	echo "ChangedCalls: $changedMethodCalls OverallCalls: $calls"
+	changedMethodCalls=$(cat $simpleTraceFolder/root-"$selectedBenchmark"* | grep  $changedMethod | wc -l)
+	calls=$(cat $simpleTraceFolder/root-"$selectedBenchmark"* | wc -l)
+	echo "$regression ChangedCalls: $changedMethodCalls OverallCalls: $calls"
+}
+
+for regression in $(cat identified.txt)
+do
+	getRegressionCalls $regression
 done > correctValues.csv
 
 for regression in $(cat ../regressions.csv | awk -F';' '{print $1}' | grep -vxFf identified.txt)
 do
-	selectedBenchmark=$(cat ../regressions.csv | grep $regression";" | awk -F';' '{print $2}')
-	changedMethod=$(cat ../regressions.csv | grep $regression";" | awk -F';' '{print $3}')
-	changedMethodCalls=$(cat $2/simpleTraces/root-"$selectedBenchmark"* | grep  $changedMethod | wc -l)
-	calls=$(cat $2/simpleTraces/root-$selectedBenchmark_* | wc -l)
-	echo "ChangedCalls: $changedMethodCalls OverallCalls: $calls"
+	getRegressionCalls $regression
 done > wrongMeasurementValues.csv
 
 echo -n "Share of changed method on correct measurements: "
-cat correctValues.csv | awk '{print $2/$4}' | getSum
+cat correctValues.csv | awk '{if ($5 != 0) {print $3/$5}}' | getSum
 
 echo -n "Share of changed method on wrong measurements: "
-cat wrongMeasurementValues.csv | awk '{print $2/$4}' | getSum
+cat wrongMeasurementValues.csv | awk '{if ($5 != 0) {print $3/$5}}' | getSum
 
--- scripts/treeCreation/analyzeFrequency.sh
+++ scripts/treeCreation/analyzeFrequency.sh
@@ -2,16 +2,30 @@ function getSum {
   awk '{sum += $1; square += $1^2} END {print sqrt(square / NR - (sum/NR)^2)" "sum/NR" "NR}'
 }
 
+treeFolder=$1
+if [ ! -d $treeFolder ]
+then
+	echo "TreeFolder was no folder: $treeFolder"
+	exit 1
+fi
+simpleTraceFolder=$treeFolder/simpleTraces
+if [ ! -d $simpleTraceFolder ]
+then
+	echo "SimpleTraceFolder was not existing: $simpleTraceFolder"
+	exit 1
+fi
+
 rm invocations.txt
 
 for line in $(cat ../regressions.csv)
 do
-	benchmark=$(echo $line | awk -F';' '{print $2}')
+	regression=$(echo $line | awk -F';' '{print $1}')
+	selectedBenchmark=$(echo $line | awk -F';' '{print $2}')
 	changedMethod=$(echo $line | awk -F';' '{print $3}')
-	changedMethodCalls=$(cat $1/simpleTraces/root-$selectedBenchmark_* | grep  $changedMethod | wc -l)
-	calls=$(cat $1/simpleTraces/root-$selectedBenchmark_* | wc -l)
-	echo "ChangedCalls: $changedMethodCalls OverallCalls: $calls"
+	changedMethodCalls=$(cat $simpleTraceFolder/root-"$selectedBenchmark"* | grep  $changedMethod | wc -l)
+	calls=$(cat $1/simpleTraces/root-"$selectedBenchmark"* | wc -l)
+	echo "$regression ChangedCalls: $changedMethodCalls OverallCalls: $calls"
 done > invocations.txt
 
 echo -n "Overall share of changed method: "
-cat invocations.txt | awk '{print $2/$4}' | getSum
+cat invocations.txt | awk '{if ($5 != 0) {print $3/$5}}' | getSum
